77d30a325576becce2286112b17987909de4729d
Mau <maufuku3009@gmail.com>
2026-08-17 14:13:10 -0600

    cambios en demo

--- public/demo-app/flutter_bootstrap.js
+++ public/demo-app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1745801332" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2080680508" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
