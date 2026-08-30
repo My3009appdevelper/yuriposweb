aaabc481888540d8120e64ecfb10e9247a1c0e06
Mau <maufuku3009@gmail.com>
2026-08-29 18:38:10 -0600

    Tienda

--- public/tienda-app/flutter_bootstrap.js
+++ public/tienda-app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3627903398" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "468893924" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
